2da4ec04cf8f30fb8ec8945c7be1ac32fb9cba15
Lim Han <lim.han@gmail.com>
2013-05-12 19:15:24 +0800

    LH :: sort of done version

--- app.js
+++ app.js
@@ -26,7 +26,10 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/', routes.experience);
+app.get('/experience', routes.experience);
+app.get('/skills', routes.skills);
+app.get('/contact', routes.contact);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,15 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Lim Han - A Collection Of Professional Experience' });
+exports.experience = function(req, res){
+  res.render('experience', { title: 'Lim Han - A Collection Of Professional Experience', headerTitle: 'Experience', active: 'experience' });
 };
+
+exports.skills = function(req, res) {
+  res.render('skills', { title: 'Lim Han - A Collection Of Professional Qualifications', headerTitle: 'Skills', active: 'skills' });
+};
+
+exports.contact = function(req, res) {
+  res.render('contact', { title: 'Lim Han - How to contact me', headerTitle: 'Contact', active: 'contact' });
+};
+
